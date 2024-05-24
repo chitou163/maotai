e729f54e554fb691dd7813cc598d241813f87dc7
chitou163 <149032092+chitou163@users.noreply.github.com>
2024-05-24 14:59:38 +0800

    Update i-maotai.js

--- i-maotai.js
+++ i-maotai.js
@@ -170,7 +170,6 @@ if ($.isNode()) {
         $.itemId = itemID;
         $.shopIds = $.stock[$.itemId];
         if (!$.shopIds) {
-          let message = ''; // 初始化 message 变量
           let msg = `❌ ${getProductInfo($.itemId, 'title')} [${$.itemId}]暂无可申购门店。\n`;
           console.log(msg);
           message += msg;
